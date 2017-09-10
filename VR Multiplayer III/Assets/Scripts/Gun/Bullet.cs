cc6177c68840116ca5faddbac971aee3f174f35f
Cody <rp17000@gmail.com>
2017-09-10 00:12:09 -0600

    Merged and Fixed

---
+++ VR Multiplayer III/Assets/Scripts/Gun/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Bullet : NetworkBehaviour {
+
+    public Transform buletStore;
+
+    private void OnEnable()
+    {
+        
+        BulletPool.bullets.Add(gameObject.GetComponent<Rigidbody>());
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        BulletPool.shooting = false;
+        //gameObject.GetComponent <Collider>().isTrigger = false;
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        gameObject.transform.position = buletStore.position;
+        BulletPool.bullets.Add(gameObject.GetComponent<Rigidbody>());
+    }
+}
