abf98f87ae955fcb34639d12f21ad73076fd0b4d
Ben <Fuzzinator12@gmail.com>
2017-09-09 20:04:06 -0600

    CC improvements & primary Weapon scripts
    the CC is just about perfect. and the primary weapon works really well.

--- VR Multiplayer III/Scripts/Mover.cs
+++ VR Multiplayer III/Scripts/Mover.cs
@@ -10,9 +10,15 @@ public class Mover : NetworkBehaviour {
 
     Controller controller;
 
+    private float newRotate;
+    private Quaternion focusRotate;
+    private Vector3 moveRotate;
+    private Rigidbody characterRigid;
+    private float forward;
 
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start ()
     {
 
         if (!isLocalPlayer)
@@ -20,15 +26,15 @@ public class Mover : NetworkBehaviour {
             Destroy(this);
             return;
         }
-            
 
+        characterRigid = gameObject.GetComponent<Rigidbody>();
         CallMover += CallMoverHandler;
     }
 
     private void CallMoverHandler(/*string command,*/ Controller mover)
     {
         controller = mover;
-        if (controller.touching)
+        if (controller.touching || controller.gripped)
         {
             StartCoroutine(MoveController());
         }
@@ -61,10 +67,46 @@ public class Mover : NetworkBehaviour {
         yield return new WaitForFixedUpdate();
         controller.device = SteamVR_Controller.Input((int)controller.trackedObject.index);
         controller.touchSpot = new Vector2(controller.device.GetAxis().x, controller.device.GetAxis().y);
-        if (controller.touchSpot != new Vector2(0, 0))
+        
+
+        
+
+        if (controller.touchSpot != new Vector2(0, 0) && !controller.gripped)
         {
-            transform.Translate(0, 0, (controller.touchSpot.y * controller.moveSpeed) * Time.deltaTime);
-            transform.Rotate(0, (controller.touchSpot.x * controller.rotateSpeed) * Time.deltaTime, 0);
+            //set input value to always be positive
+            if (controller.touchSpot != null)
+            {
+                forward = controller.device.GetAxis().x + controller.device.GetAxis().y;
+                if (forward < 0)
+                {
+                    forward *= -1;
+                }
+                if (forward > 1)
+                {
+                    forward *= .5f;
+                }
+            }
+
+            //controls transformations
+            characterRigid.MovePosition(transform.localPosition + transform.TransformDirection(new Vector3(/*controller.touchSpot.x*/0 , 0, forward/*controller.touchSpot.y*/)) * controller.moveSpeed * Time.deltaTime);
+
+            //controls rotation
+            newRotate = Mathf.Atan2(controller.device.GetAxis().y, (controller.device.GetAxis().x *-1)) * Mathf.Rad2Deg + 90;
+            Quaternion tempRotate = Quaternion.Euler(0, newRotate, 0);    
+            transform.localRotation = Quaternion.Slerp(transform.rotation, tempRotate, Time.deltaTime * controller.rotateSpeed);
+
+        }
+
+        if (controller.gripped)
+        {
+            //controlls rotation
+            focusRotate = Quaternion.LookRotation(controller.focus.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, focusRotate, Time.deltaTime * controller.rotateSpeed);
+            transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+            //controlls transformation
+            characterRigid.MovePosition(transform.localPosition + transform.TransformDirection
+                (new Vector3(controller.touchSpot.x, 0, controller.touchSpot.y)) * controller.moveSpeed * Time.deltaTime);
+
         }
         if (controller.touching)
             StartCoroutine(MoveController());
